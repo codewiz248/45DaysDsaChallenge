202c0b5774f44da18690a8d1f1961a985e75a60b
codewiz248 <maindsakshi001@gmail.com>
2026-05-26 08:48:50 +0530

    save chnages

---
+++ sizeof.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age = 20;
+    float f = 34.84894485;
+    double pi = 9.9694969466944964;
+    char grade = 'A';
+
+    cout << sizeof(age) << endl;
+    cout << sizeof(f) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(char) << endl;
+
+    return 0;
+}
