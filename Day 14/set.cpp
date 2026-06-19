959433485a32245d225f116d7fe1e57cea560796
codewiz248 <maindsakshi001@gmail.com>
2026-06-19 10:58:09 +0530

    day 14

---
+++ Day 14/set.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set<int, greater<int>> s; //greater makes sorting in descending order
+    s.insert(2);
+    s.insert(2);
+    s.insert(2);
+    s.insert(4);
+    s.insert(6);
+    s.insert(8);
+    s.insert(81);
+    s.insert(12);
+
+    cout << s.count(2) << endl;
+    cout << s.count(82) << endl;
+
+    for (auto it : s)
+    {
+        cout << it << " ";
+    }
+
+    cout << endl;
+
+    auto it = s.find(10); // point toward address of 10 or end address
+    if (it != s.end())
+    {
+        cout << "found";
+    }
+    else
+    {
+        cout << "not found\n";
+    }
+
+    s.erase(81);
+   s.erase(s.begin());
+    for (auto it : s)
+    {
+        cout << it << " ";
+    }
+}
