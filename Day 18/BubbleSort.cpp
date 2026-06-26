e44f57c04897e7dcd44d684a7a2717156ea04f31
codewiz248 <maindsakshi001@gmail.com>
2026-06-26 22:28:45 +0530

    progress update

--- Day 18/BubbleSort.cpp
+++ Day 18/BubbleSort.cpp
@@ -7,7 +7,6 @@ void BubbleSort(vector<int> &arr)
 
     for (int i = 0; i < arr.size() - 1; i++)
     {
-
         for (int j = 0; j < arr.size() - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -22,7 +21,6 @@ int main()
 {
     vector<int> arr = {12, 45, 23, 50, 20, 8};
     BubbleSort(arr);
-
     for (auto it : arr)
     {
         cout << it << " ";
