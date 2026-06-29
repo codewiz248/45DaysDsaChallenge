5618e092426b3b6f515e12174041e692ebbc86d8
codewiz248 <maindsakshi001@gmail.com>
2026-06-29 14:31:34 +0530

    files added

--- Day 19/MergeSort.cpp
+++ Day 19/MergeSort.cpp
@@ -2,28 +2,62 @@
 #include <vector>
 using namespace std;
 
-class solution
+void mergeArray(vector<int> &arr, int low, int mid, int high)
 {
-public:
-    void insertionSort(vector<int> &arr)
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high)
     {
-        for (int i = 1; i < arr.size(); i++)
+        if (arr[left] <= arr[right])
         {
-            for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--)
-            {
-                swap(arr[j - 1], arr[j]);
-            }
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
         }
     }
-};
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low == high)
+    {
+        return;
+    }
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    mergeArray(arr, low, mid, high);
+}
 
 int main()
 {
-    vector<int> arr = {1, 2, 42, 33, 12, 54, 11, 5};
-    solution s1;
-    s1.insertionSort(arr);
-    for (auto it : arr)
-    {
-        cout << it << " ";
-    }
+    vector<int> v = {10, 9, 6, 8, 7, 4, 2, 5, 3, 1,11,21,32,45};
+    mergeSort(v, 0, v.size() - 1);
+
+    for(auto it : v){
+        cout<<it<<" ";
+    }    
+    return 0;
 }
--- Day 19/insertionSort.cpp
+++ Day 19/insertionSort.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
+class solution
+{
+public:
+    void insertionSort(vector<int> &arr)
+    {
+        for (int i = 1; i < arr.size(); i++)
+        {
+            for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--)
+            {
+                swap(arr[j - 1], arr[j]);
+            }
+        }
+    }
+};
 
 int main()
 {
-    
+    vector<int> arr = {1, 2, 42, 33, 12, 54, 11, 5};
+    solution s1;
+    s1.insertionSort(arr);
+    for (auto it : arr)
+    {
+        cout << it << " ";
+    }
 }
